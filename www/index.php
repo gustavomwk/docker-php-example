52e25d479a44eaedc5cc9bedda58480faa969895
gustavo waku <gustavo.waku@ifms.edu.br>
2022-07-26 11:50:26 -0400

    primeira versao docker funcionando

---
+++ www/index.php
@@ -0,0 +1,4 @@
+<?php 
+  echo "<pre><h2>Criando um ambiente de desenvolvimento PHP com Docker Compose.</h2></pre><br/>";
+  mysqli_connect("db", "root", "phprs") or die(mysqli_error()); 
+  echo "<pre><h3>Connected to MySQL Eba Funcinou!<h3></pre><br/>"; 
